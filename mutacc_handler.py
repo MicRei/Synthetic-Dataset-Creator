c795e39a1952492f99d6e5db9958cc703be90ba1
MicRei <mickaelreinman@hotmail.com>
2019-04-16 17:13:40 +0200

    Added comments on mutacc_handler

--- mutacc_handler.py
+++ mutacc_handler.py
@@ -1,14 +1,7 @@
 """
 File to handle mutacc interaction.
 """
-
-import os
-import numpy as np
-import re
-import sys
-import pysam as ps
 import yaml
-import mutacc as mac
 import subprocess as sp
 from pathlib import Path
 
@@ -24,10 +17,11 @@ def import_to_database(case_id, configfile, *args):
     """
     Function to create a new data set, or use existing data, and insert it into the mutacc database
 
-    :param case_id: path to a yamlfile with the correct structure or the ID of a new case
-    :param args: the 8 additional data needed to create a new case;
-                    sample_id, sex, mother, father, bam, analysis, phenotype, variants
-    :return: None
+    :param case_id:     path to a yamlfile with the correct structure or the ID of a new case
+    :param configfile:  config file containing the root directory of mutaccfiles
+    :param args:        the 8 additional data needed to create a new case;
+                            sample_id, sex, mother, father, bam, analysis, phenotype, variants
+    :return:            None
     """
 
     try:
@@ -36,24 +30,19 @@ def import_to_database(case_id, configfile, *args):
                 if not yaml_case.readable():
                     raise MutaccError("No read permission granted.")
                 else:
+                    print("Importing into database")
                     # TODO: Add --config-file <config_file> and
                     #  --padding NUMBER to extract subprocess. Make arguments in extract dynamic.
-                    # sp.run(['mutacc', '--config', configfile, 'extract', '--case', yaml_case.name])
-
+                    _mutacc_extract_and_import(configfile, yaml_case.name)
                     # TODO: Add a config file or rootdir to mutacc import subprocess
-                    # sp.run(['mutacc', 'db', 'import', 'root_dir/imports/' + yaml_case.name + '.mutacc'])
         elif len(args) == 8:
             new_data = [case_id]
 
             for data in args:
                 new_data.append(data)
             file_name = _create_yaml_file(*new_data)
-            # TODO: Add --config-file <config_file> and
-            #  --padding NUMBER to extract subprocess. Make arguments in extract dynamic.
-            # sp.run(['mutacc', '--config', configfile, 'extract', '--case', file_name])
+            _mutacc_extract_and_import(configfile, file_name)
 
-            # TODO: Add a config file or rootdir to mutacc import subprocess
-            # sp.run(['mutacc', 'db', 'import', 'root_dir/imports/' + file_name + '.mutacc'])
         else:
             raise MutaccError("Not enough args sent to import or no such file exists. Please supplement your data")
 
@@ -61,26 +50,36 @@ def import_to_database(case_id, configfile, *args):
         print("Something went wrong during import: ", e)
 
 
+# TODO: Add --config-file <config_file> and
+#  --padding NUMBER to extract subprocess. Make arguments in extract dynamic.
+# TODO: Add a config file or rootdir to mutacc import subprocess
+def _mutacc_extract_and_import(configfile, filename):
+    """
+    Internal function to handle mutacc data extraction and import to database
+
+    :param configfile:      the config file containing the root directory of mutaccfiles
+    :param filename:        The name of the case file
+    :return:                None
+    """
+    sp.run(['mutacc', '--config-file', configfile, 'extract', '--case', filename])
+    sp.run(['mutacc', 'db', 'import', '/.../root_dir/imports/' + filename + '.mutacc'])
+
+
 # TODO:
-#   Return wanted sample from database
-#   use subprocess module for command line "mutacc --config-file <config.yaml> db export -m affected -c '{}'"
-# def export_from_database(case):
-
-
-# TODO: export case and if that is successful, call create_dataset_from_case
-
-
-# TODO:
-#   Create the dataset from exported case
-#   use subprocess module for command line
-#   "mutacc --config-file <config_file> synthesize -b <bam> -f <fastq1_child> -f2 <fastq2_child> -q child_query.mutacc"
-# def create_dataset_from_case(data):
+#   Return wanted sample from database and Create the dataset from exported case
+#   Add more options or make options dynamic?
+#   Make config file dynamic
+def export_from_database(case):
+    sp.run(['mutacc', '--config-file', './mutacc_config.yaml', 'db', 'export', '-m', 'affected', '-c', '{}'])
+    sp.run(['mutacc', '--config-file', './mutacc_config.yaml', 'synthesize', '-b', '<bam>', '-f', '<fastq1_child>', '-f2',
+            '<fastq2_child>', '-q', 'child_query.mutacc'])
 
 
 # TODO:
 #   Remove specific case form database
-#   use subprocess module for command line "mutacc --config-file <config.yaml> db remove <case_id>"
-# def remove_from_database(case):
+#   Make config file dynamic
+def remove_from_database(case):
+    sp.run(['mutacc', '--config-file', './mutacc_config.yaml', 'db', 'remove', case])
 
 
 # TODO:
@@ -91,7 +90,18 @@ def import_to_database(case_id, configfile, *args):
 
 def _create_yaml_file(case_id, sample_id, sex, mother, father, bam, analysis, phenotype, variants):
     """
-        Internal function to create a YAML document from provided data.
+    Internal function to create a YAML document from provided data.
+
+    :param case_id:     name of the case
+    :param sample_id:   name for the sample
+    :param sex:         gender of patient
+    :param mother:      sample id of mother, if applicable
+    :param father:      sample id of father, if applicable
+    :param bam:         path to bamfile containing reads for patient
+    :param analysis:    analysis type performed on patients data
+    :param phenotype:   phenotype of patient (affected or unaffected)
+    :param variants:    path to variantfile patient
+    :return:            The name of the created file
     """
     if case_id is str:
         create_file = case_id + ".yaml"
