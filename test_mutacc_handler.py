a0d66291c0d3ea7d06e2a2a7645cd240822ad590
MicRei <mickaelreinman@hotmail.com>
2019-04-16 09:45:09 +0200

    Working on YAML creation

--- test_mutacc_handler.py
+++ test_mutacc_handler.py
@@ -1,4 +1,5 @@
 import unittest
+import sys
 
 from setuptools.command.test import test
 
@@ -11,7 +12,7 @@ class TestMutaccHandler(unittest.TestCase):
 
     def test_YAML_loading(self):
         #yamlfile = self.assertmutacc_handler._create_YAML_file('22655', 'female', 'sample3', '0', './here.bam') # add as arguments --> id, sex, mother, father, bam
-        self.assertFalse(True, msg='Hello there ;)')
+        #self.assertis(read('yamlfile', 'r'), msg='Hello there ;)')
 
 if __name__ == '__main__':
     unittest.main()
