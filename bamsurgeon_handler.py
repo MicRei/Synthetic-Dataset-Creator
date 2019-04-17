97c7d01aa7205fd6128c9f4e88a171e1d21b6d73
MicRei <mickaelreinman@hotmail.com>
2019-04-17 12:48:46 +0200

    Started work on bamsurgeon handler

--- bamsurgeon_handler.py
+++ bamsurgeon_handler.py
@@ -32,8 +32,8 @@ import subprocess as sp
 #   run subprocess module with "addsnv  -v REGIONS_TO_MUTATE -r REFERENCE_FASTA -f BAM_FILE -o OUTPUT_FILE"
 def create_snv(addsnv_location, variationfile, referencefile, bamfile, outputfile, nr_procs, *args):
     sp.run(
-        [addsnv_location, '-v', variationfile, '-r', referencefile, '-f', bamfile, '-o', outputfile, '-p',
-         nr_procs])
+        [addsnv_location, '-v', variationfile, '-r', referencefile, '-f', bamfile, '-o', outputfile,
+         '-p', nr_procs, '--maxdepth', '10000'])
 
 
 # TODO:
@@ -41,7 +41,7 @@ def create_snv(addsnv_location, variationfile, referencefile, bamfile, outputfil
 def create_sv(addsv_location, variationfile, referencefile, bamfile, outputfile, nr_procs, *args):
     print("HEllo from indel)")
     sp.run([addsv_location, '-v', variationfile, '-r', referencefile, '-f', bamfile, '-o', outputfile, '-p',
-            nr_procs])
+            nr_procs, '--maxdepth', '10000'])
 
 
 # TODO:
@@ -50,4 +50,4 @@ def create_indel(addindel_location, variationfile, referencefile, bamfile, outpu
     print("HEllo from indel)")
     sp.run(
         [addindel_location, '-v', variationfile, '-r', referencefile, '-f', bamfile, '-o', outputfile, '-p',
-         nr_procs])
+         nr_procs, '--maxdepth', '10000'])
--- test_bamsurgeon_handler.py
+++ test_bamsurgeon_handler.py
@@ -8,8 +8,8 @@ class MyTestCase(unittest.TestCase):
         variationfile = '/home/mire/PycharmProjects/project_files/mychr17snv.bed'
         referencefile = '/home/mire/PycharmProjects/project_files/fastadocs/hg19.fa'
         bamfile = '/home/mire/PycharmProjects/project_files/' \
-                  '17-1143_S26_R_001_nSCLC_NORMAL.molbar.trimmed.deduped.merged.sorted.bam'
-        outputfile = '/home/mire/PycharmProjects/project_files/addsnvtest.bam'
+                  '17-1143_mybam.sorted.bam'
+        outputfile = '/home/mire/PycharmProjects/project_test/bms_output/addsnvtest.bam'
         nr_procs = '30'
 
         bams.create_snv(addsnv_location, variationfile, referencefile, bamfile, outputfile, nr_procs)
