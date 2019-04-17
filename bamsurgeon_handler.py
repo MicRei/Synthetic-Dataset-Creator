0843071088be010afea92c254e42cd84a3690a96
MicRei <mickaelreinman@hotmail.com>
2019-04-17 11:24:59 +0200

    Started work on bamsurgeon handler

--- bamsurgeon_handler.py
+++ bamsurgeon_handler.py
@@ -30,17 +30,34 @@ import subprocess as sp
 
 # TODO:
 #   run subprocess module with "addsnv  -v REGIONS_TO_MUTATE -r REFERENCE_FASTA -f BAM_FILE -o OUTPUT_FILE"
-def create_snv(addsnv_location, variationfile, referencefile, bamfile, outputfile, *args):
-    sp.run(['addsnv.py', '-v', 'REGIONS_TO_MUTATE', '-r', 'REFERENCE_FASTA', '-f', 'BAM_FILE', '-o', 'OUTPUT_FILE'])
+def create_snv():  # addsnv_location, variationfile, referencefile, bamfile, outputfile, nr_procs, *args)
+
+    result = 'nothing found'
+    for root, dirs, files in os.walk('/home/'):
+        if 'addsnv.py' in files:
+            result = os.path.join(root, 'addsnv.py')
+
+    print(result)
+    # sp.run(
+    #    ['addsnv.py', '-v', variationfile, '-r', referencefile, '-f', bamfile, '-o', outputfile, '-p',
+    #     nr_procs])
 
 
 # TODO:
 #   run subprocess module with "addsv  -v REGIONS_TO_MUTATE -r REFERENCE_FASTA -f BAM_FILE -o OUTPUT_FILE"
-def create_sv(addsv_location, variationfile, referencefile, bamfile, outputfile, *args):
-    sp.run(['addsv.py', '-v', 'REGIONS_TO_MUTATE', '-r', 'REFERENCE_FASTA', '-f', 'BAM_FILE', '-o', 'OUTPUT_FILE'])
+def create_sv(addsv_location, variationfile, referencefile, bamfile, outputfile, nr_procs, *args):
+    print("HEllo from indel)")
+
+
+# sp.run(['addsv.py', '-v', variationfile, '-r', referencefile, '-f', bamfile, '-o', outputfile, '-p',
+#     nr_procs])
 
 
 # TODO:
 #   run subprocess module with "addindel  -v REGIONS_TO_MUTATE -r REFERENCE_FASTA -f BAM_FILE -o OUTPUT_FILE"
-def create_indel(addindel_location, variationfile, referencefile, bamfile, outputfile, *args):
-    sp.run(['addindel.py', '-v', 'REGIONS_TO_MUTATE', '-r', 'REFERENCE_FASTA', '-f', 'BAM_FILE', '-o', 'OUTPUT_FILE'])
+def create_indel(addindel_location, variationfile, referencefile, bamfile, outputfile, nr_procs, *args):
+    print("HEllo from indel)")
+
+# sp.run(
+#    ['addindel.py', '-v', variationfile, '-r', referencefile, '-f', bamfile, '-o', outputfile, '-p',
+#     nr_procs])
--- test_bamsurgeon_handler.py
+++ test_bamsurgeon_handler.py
@@ -1,19 +1,22 @@
 import unittest
-
+import bamsurgeon_handler as bams
 
 class MyTestCase(unittest.TestCase):
+    def test_create_snv(self):
+        bams.create_snv()
+"""
+
     def test_something(self):
         self.assertEqual(True, False)
 
-    def test_create_snv(self):
-        self.assertEqual(True, False)
+
 
     def test_create_sv(self):
         self.assertEqual(True, False)
 
     def test_create_indel(self):
         self.assertEqual(True, False)
-
+"""
 
 if __name__ == '__main__':
     unittest.main()
