9a6d0af7164d09e2097dd7cbb5f081f58546afbf
MicRei <mickaelreinman@hotmail.com>
2019-04-17 12:22:43 +0200

    Started work on bamsurgeon handler

--- test_bamsurgeon_handler.py
+++ test_bamsurgeon_handler.py
@@ -1,10 +1,20 @@
 import unittest
 import bamsurgeon_handler as bams
 
+
 class MyTestCase(unittest.TestCase):
     def test_create_snv(self):
-        bams.create_snv()
-"""
+        addsnv_location = '/home/mire/.local/bin/addsnv.py'
+        variationfile = '/home/mire/PycharmProjects/project_files/mychr17snv.bed'
+        referencefile = '/home/mire/PycharmProjects/project_files/fastadocs/hg19.fa'
+        bamfile = '/home/mire/PycharmProjects/project_files/' \
+                  '17-1143_S26_R_001_nSCLC_NORMAL.molbar.trimmed.deduped.merged.sorted.bam'
+        outputfile = '/home/mire/PycharmProjects/project_files/addsnvtest.bam'
+        nr_procs = '30'
+
+        bams.create_snv(addsnv_location, variationfile, referencefile, bamfile, outputfile, nr_procs)
+
+    """
 
     def test_something(self):
         self.assertEqual(True, False)
@@ -16,7 +26,8 @@ class MyTestCase(unittest.TestCase):
 
     def test_create_indel(self):
         self.assertEqual(True, False)
-"""
+    """
+
 
 if __name__ == '__main__':
     unittest.main()
