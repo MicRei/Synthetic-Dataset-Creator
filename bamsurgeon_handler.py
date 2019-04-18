3ccede9e57397f6ede596ed86073262329fdfb8f
MicRei <mickaelreinman@hotmail.com>
2019-04-18 10:34:49 +0200

    User handler prototype kind of done, bamsurgeon update

--- bamsurgeon_handler.py
+++ bamsurgeon_handler.py
@@ -5,6 +5,24 @@ File to handle bamsurgeon interaction.
 import subprocess as sp
 
 
+def create_mutations(mutationtype, variationfile, referencefile, bamfile, outputfile, nr_procs, *args):
+    """
+        Create mutation based on userinput in bamfile data based on positions given in variationfile and print
+         them to outputfile.
+
+        :param variationfile:   BED file specifying position to mutate, optionally with the desired base.
+        :param referencefile:   FASTA file containing the genome the sample is mapped against.
+        :param bamfile:         BAM file containing the sample reads.
+        :param outputfile:      BAM file to store the mutated data.
+        :param nr_procs:        Number of processes/threads to run the program with.
+        :param args:            Additional arguments, not implemented.
+        :return:                None.
+        """
+    sp.run(
+        [mutationtype, '-v', variationfile, '-r', referencefile, '-f', bamfile, '-o', outputfile,
+         '-p', nr_procs, '--maxdepth', '10000'])
+
+
 # TODO:
 #   Add more arguments if desired by the user
 def create_snv(variationfile, referencefile, bamfile, outputfile, nr_procs, *args):
--- user_handler.py
+++ user_handler.py
@@ -7,14 +7,43 @@ import numpy as np
 import re
 import sys
 import pysam as ps
-import mutacc as mac
 import time
-import mutacc_handler
+import mutacc_handler as mach
+import bamsurgeon_handler as bamh
 import argparse
 
 
-
-#TODO:
+# TODO:
 #   Figure out what arguments to use.
 #   Add a parser that reads all arguments and stores them.
 #   Allow the stored arguments to be used by the correct function.
+
+# TODO:
+#   Function that calls bamsurgeon and creates mutations based on arguments received
+def create_mutations_in_bamfile():
+    bamh.create_mutations()
+
+
+# TODO:
+#   Function that calls mutacc and adds a bam file and case to the database
+def add_case_to_database():
+    mach.import_to_database()
+
+
+# TODO:
+#   Function to remove case from database
+def remove_case_from_database():
+    mach.remove_from_database()
+
+
+# TODO:
+#   Function to create mutation and add it directly to the database
+def bundle_to_database():
+    create_mutations_in_bamfile()
+    add_case_to_database()
+
+
+# TODO:
+#   Function to retrieve a case from the database and synthesize it
+def create_dataset():
+    mach.export_from_database()
