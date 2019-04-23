69e8b4ced43d6d35902c425ffcc3f76ca15dc666
MicRei <mickaelreinman@hotmail.com>
2019-04-23 13:16:51 +0200

    Working on user handler. Added parameters to methods and arguments to called methods.

--- bamsurgeon_handler.py
+++ bamsurgeon_handler.py
@@ -4,7 +4,7 @@ File to handle bamsurgeon interaction.
 
 import subprocess as sp
 
-
+# TODO: Parse args in case of more arguments. REMEMBER: addsv does not handle picard.
 def create_mutations(mutationtype, variationfile, referencefile, bamfile, outputfile, nr_procs, *args):
     """
         Create mutation based on userinput in bamfile data based on positions given in variationfile and print
@@ -21,64 +21,3 @@ def create_mutations(mutationtype, variationfile, referencefile, bamfile, output
     sp.run(
         [mutationtype, '-v', variationfile, '-r', referencefile, '-f', bamfile, '-o', outputfile,
          '-p', nr_procs, '--maxdepth', '10000'])
-
-
-# TODO:
-#   Add more arguments if desired by the user
-def create_snv(variationfile, referencefile, bamfile, outputfile, nr_procs, *args):
-    """
-    Create new single nucleotide variations in bamfile data based on positions given in variationfile and print
-     them to outputfile.
-
-    :param variationfile:   BED file specifying position to mutate, optionally with the desired base.
-    :param referencefile:   FASTA file containing the genome the sample is mapped against.
-    :param bamfile:         BAM file containing the sample reads.
-    :param outputfile:      BAM file to store the mutated data.
-    :param nr_procs:        Number of processes/threads to run the program with.
-    :param args:            Additional arguments, not implemented.
-    :return:                None.
-    """
-    sp.run(
-        ['addsnv.py', '-v', variationfile, '-r', referencefile, '-f', bamfile, '-o', outputfile,
-         '-p', nr_procs, '--maxdepth', '10000'])
-
-
-# TODO:
-#   Add more arguments if desired by the user
-def create_sv(variationfile, referencefile, bamfile, outputfile, nr_procs, *args):
-    """
-    Create new structural variations in bamfile data based on positions given in variationfile and print
-     them to outputfile. Very time consuming.
-
-    :param variationfile:   BED file specifying position or region to mutate, with desired mutation type,
-                            allelic fraction, and desired bases mutate to specified.
-    :param referencefile:   FASTA file containing the genome the sample is mapped against.
-    :param bamfile:         BAM file containing the sample reads.
-    :param outputfile:      BAM file to store the mutated data.
-    :param nr_procs:        Number of processes/threads to run the program with.
-    :param args:            Additional arguments, not implemented.
-    :return:                None.
-    """
-    sp.run(['addsnv.py', '-v', variationfile, '-r', referencefile, '-f', bamfile, '-o', outputfile, '-p',
-            nr_procs, '--maxdepth', '10000'])
-
-
-# TODO:
-#   Add more arguments if desired by the user
-def create_indel(variationfile, referencefile, bamfile, outputfile, nr_procs, *args):
-    """
-    Create inserts and deletions in bamfile data based on positions given in variationfile and print
-     them to outputfile.
-
-    :param variationfile:   BED file specifying position or region to mutate, with the desired mutation type
-                            and, for insertions, base specified.
-    :param referencefile:   FASTA file containing the genome the sample is mapped against.
-    :param bamfile:         BAM file containing the sample reads.
-    :param outputfile:      BAM file to store the mutated data.
-    :param nr_procs:        Number of processes/threads to run the program with.
-    :param args:            Additional arguments, not implemented.
-    :return:                None.
-    """
-    sp.run(
-        ['addsnv.py', '-v', variationfile, '-r', referencefile, '-f', bamfile, '-o', outputfile, '-p',
-         nr_procs, '--maxdepth', '10000'])
--- user_handler.py
+++ user_handler.py
@@ -20,27 +20,39 @@ import argparse
 
 # TODO:
 #   Function that calls bamsurgeon and creates mutations based on arguments received
-def create_mutations_in_bamfile():
-    bamh.create_mutations()
+def create_mutations_in_bamfile(mutationtype, variationfile, referencefile, bamfile, outputfile, nr_procs):
+    bamh.create_mutations(mutationtype, variationfile, referencefile, bamfile, outputfile, nr_procs)
 
 
 # TODO:
 #   Function that calls mutacc and adds a bam file and case to the database
-def add_case_to_database():
-    mach.import_to_database()
+def import_to_database(case_id, configfile, *args):
+    mach.import_to_database(case_id, configfile, *args)
 
 
 # TODO:
 #   Function to remove case from database
-def remove_case_from_database():
-    mach.remove_from_database()
+def remove_case_from_database(case_id):
+    mach.remove_from_database(case_id)
 
 
 # TODO:
 #   Function to create mutation and add it directly to the database
-def mass_import_to_database():
-    create_mutations_in_bamfile()
-    add_case_to_database()
+def mass_import_to_database(mutationtype, list_of_variationfiles, list_of_referencefiles, list_of_bamfiles,
+                            list_of_outputfiles, nr_procs, list_of_case_ids, configfile, *matrix_of_args):
+    mutationtype, nr_procs, configfile = mutationtype, nr_procs, configfile
+
+    # TODO: loop through all the files below and call create_mutations + import_to_database for each file/case.
+
+    for case in list_of_case_ids:
+        case_id = list_of_case_ids(case)
+        variationfile = list_of_variationfiles(case)
+        referencefile = list_of_referencefiles(case)
+        bamfile = list_of_bamfiles(case)
+        outputfile = list_of_outputfiles(case)
+        args = matrix_of_args(case)
+        create_mutations_in_bamfile(mutationtype, variationfile, referencefile, bamfile, outputfile, nr_procs)
+        import_to_database(case_id, configfile, *args)
 
 
 # TODO:
