f78989eae4fa2d58d78c853916a204edbe5258ef
MicRei <mickaelreinman@hotmail.com>
2019-05-08 16:27:09 +0200

    Testing of build_randomized_dataset

--- test_scripts/test_build_randomized_dataset.py
+++ test_scripts/test_build_randomized_dataset.py
@@ -8,8 +8,17 @@ class MyTestCase(unittest.TestCase):
 
     def test_get_root_dir(self):
         self.assertEqual(
-            build_dataset._get_root_dir_path('/home/mire/PycharmProjects/project_test/mutacc_config_files/mutacc_config.yaml'),
+            build_dataset._get_root_dir_path(
+                '/home/mire/PycharmProjects/project_test/mutacc_config_files/mutacc_config.yaml'),
             '/home/mire/PycharmProjects/project_test/mutacc_tests/')
+
+    def test_extract_case_ids(self):
+        caselist = build_dataset._extract_case_ids(
+            '/home/mire/PycharmProjects/project_test/mutacc_config_files/mutacc_config.yaml')
+        print(caselist)
+        self.assertIsNotNone(caselist)
+
+    def test_pair_reference_fastq_files(self):
 
 
 if __name__ == '__main__':
