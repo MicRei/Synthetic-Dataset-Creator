c0c5ce75d19437a6c4a27e5141a3337865c0da8e
MicRei <mickaelreinman@hotmail.com>
2019-05-09 17:11:44 +0200

    More testing of build_Randomized_dataset.

--- Synthetic_Dataset_Creator/build_randomized_dataset.py
+++ Synthetic_Dataset_Creator/build_randomized_dataset.py
@@ -8,6 +8,11 @@ from Synthetic_Dataset_Creator import mutacc_handler as muth
 import random
 from os import listdir
 from yaml import safe_load
+from pathlib import Path
+
+
+class BuildingError(Exception):
+    pass
 
 
 def create_randomized_dataset(case_db_configfile, synth_db_configfile, background_bam, background_fastq1,
@@ -18,20 +23,32 @@ def create_randomized_dataset(case_db_configfile, synth_db_configfile, backgroun
     :param case_db_configfile:  Configfile with path to root directory of mutacc.
     :param synth_db_configfile: Configfile with path to root directory of mutacc and name of database for synthesizing.
     :param background_bam:      BAM to use as background for dataset.
-    :param background_fastq1:   First FastQ to use as background for dataset
+    :param background_fastq1:   First FastQ to use as background for dataset.
     :param background_fastq2:   Second FastQ to use as background for dataset, pair-ended analysis.
     :param reference_data_fq1:  Reference data to add to the sampling pool. FastQ , pairended format.
-    :param reference_data_fq2:  Reference data to add to the sampling pool. Complementary list to reference_Data_fq1.
+                                    Either a file with a 1 (one) file per row or a list of filenames.
+    :param reference_data_fq2:  Reference data to add to the sampling pool.
+                                    Complementary list to reference_data_fq1 in the same format as reference_data_1.
     :return: None, synthesizes a dataset
     """
-    caselist = _extract_case_ids(case_db_configfile)
-    # case_id_list = _extract_case_ids(case_db_configfile)
-    # caselist = []
-    # caselist.extend(case_id_list)
-
+    case_id_list = _extract_case_ids(case_db_configfile)
+    caselist = []
+    caselist.extend(case_id_list)
     if reference_data_fq1 is None:
         reference_data_fq1 = []
         reference_data_fq2 = []
+    elif type(reference_data_fq1) is list:
+        print("Using List")
+        # TODO: Use list
+    elif Path(reference_data_fq1).is_file() is True:
+        print("Handling File")
+        reference_data_fq1 = [reference_data_fq1, ]
+        reference_data_fq2 = [reference_data_fq2, ]
+        # TODO: read one line at a time and add to list
+    else:
+        raise BuildingError('Reference data is not a valid file or list. Please see API for proper usage.')
+        # TODO: end run
+        return
 
     if len(reference_data_fq1) != 0 and len(reference_data_fq1) == len(reference_data_fq2):
         dict_of_referencedata = _pair_reference_fastq_files(reference_data_fq1, reference_data_fq2)
@@ -44,7 +61,7 @@ def create_randomized_dataset(case_db_configfile, synth_db_configfile, backgroun
     else:
         randomized_list = random.sample(caselist, int(len(caselist) * 0.67))
 
-    _create_synthesized_dataset_from_database(background_bam, background_fastq1, background_fastq2, caselist,
+    _create_synthesized_dataset_from_database(background_bam, background_fastq1, background_fastq2, case_id_list,
                                               case_db_configfile, randomized_list, synth_db_configfile)
 
     chosen_references = []
@@ -61,12 +78,19 @@ def create_randomized_dataset(case_db_configfile, synth_db_configfile, backgroun
     # TODO: Or just use reference data as a dataset? Or use reference data as background?
 
     root_dir = _get_root_dir_path(synth_db_configfile)
-    path_to_synthetic_datasets = root_dir + 'datasets/'
-    synthetic_fqs = listdir(path_to_synthetic_datasets)
+    path_to_synthetic_datasets = root_dir + 'synthetics/'
+    if not Path(path_to_synthetic_datasets).is_dir():
+        Path(path_to_synthetic_datasets).mkdir()
+    path_to_mutacc_datasets = root_dir + 'datasets/'
+    synthetic_fqs = listdir(path_to_mutacc_datasets)
     if len(chosen_references) > 0:
         for fq_file in range(len(chosen_references)):
-            sp.run(['cat', synthetic_fqs[0], chosen_references[fq_file]])
-            sp.run(['cat', synthetic_fqs[1], chosen_references_pair[fq_file]])
+            with open(path_to_synthetic_datasets + Path(chosen_references[fq_file]).name + '.dataset_1.fastq.gz',
+                      'w') as synthetic_fq_1, open(
+                    path_to_synthetic_datasets + Path(chosen_references[fq_file]).name + '.dataset_2.fastq.gz',
+                    'w') as synthetic_fq_2:
+                sp.run(['cat', synthetic_fqs[0], chosen_references[fq_file]], stdout=synthetic_fq_1)
+                sp.run(['cat', synthetic_fqs[1], chosen_references_pair[fq_file]], stdout=synthetic_fq_2)
 
 
 def _create_synthesized_dataset_from_database(background_bam, background_fastq1, background_fastq2, caselist,
--- test_scripts/test_build_randomized_dataset.py
+++ test_scripts/test_build_randomized_dataset.py
@@ -6,44 +6,44 @@ test_path = '/home/mire/PycharmProjects/project_test/testfiles/'
 
 
 class MyTestCase(unittest.TestCase):
-
-    def test_get_root_dir(self):
-        self.assertEqual(
-            build_dataset._get_root_dir_path(
-                test_path + 'mutacc_config_files/mutacc_config.yaml'),
-            '/home/mire/PycharmProjects/project_test/mutacc_tests/')
-
-    def test_extract_case_ids(self):
-        caselist = build_dataset._extract_case_ids(test_path + 'mutacc_config_files/mutacc_config.yaml')
-        print(caselist)
-        self.assertIsNotNone(caselist)
-        self.assertTrue('hannahmontana' in caselist)
-
-    def test_pair_reference_fastq_files(self):
-        testlist_1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p']
-        testlist_2 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', ',11', '12', '13', '14', '15', '16']
-        testdict_1 = {}
-
-        for fq_pairs in range(len(testlist_1)):
-            testdict_1[testlist_1[fq_pairs]] = testlist_2[fq_pairs]
-
-        methoddict_1 = build_dataset._pair_reference_fastq_files(testlist_1, testlist_2)
-
-        self.assertEqual(testdict_1, methoddict_1,
-                         "Dictionaries of equal size and containing equal long lists are not equals!")
-
-    def test_create_synthesized_dataset_from_database(self):
-        background_bam = test_path + 'hg38/NA12878_mybam.sorted.bam'
-        background_fastq1 = test_path + 'mutacc_NA12878_mybam_R1.fastq.gz'
-        background_fastq2 = test_path + 'mutacc_NA12878_mybam_R2.fastq.gz'
-        caselist = build_dataset._extract_case_ids(test_path + 'mutacc_config_files/mutacc_config.yaml')
-        case_db_configfile = test_path + 'mutacc_config_files/mutacc_config.yaml'
-        synth_db_configfile = test_path + 'mutacc_config_files/mutacc_synthDB_config.yaml'
-        randomized_list = random.sample(caselist, int(len(caselist) * 0.67))
-        build_dataset._create_synthesized_dataset_from_database(background_bam, background_fastq1, background_fastq2,
-                                                                caselist,
-                                                                case_db_configfile, randomized_list,
-                                                                synth_db_configfile)
+    #
+    # def test_get_root_dir(self):
+    #     self.assertEqual(
+    #         build_dataset._get_root_dir_path(
+    #             test_path + 'mutacc_config_files/mutacc_config.yaml'),
+    #         '/home/mire/PycharmProjects/project_test/mutacc_tests/')
+    #
+    # def test_extract_case_ids(self):
+    #     caselist = build_dataset._extract_case_ids(test_path + 'mutacc_config_files/mutacc_config.yaml')
+    #     print(caselist)
+    #     self.assertIsNotNone(caselist)
+    #     self.assertTrue('hannahmontana' in caselist)
+    #
+    # def test_pair_reference_fastq_files(self):
+    #     testlist_1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p']
+    #     testlist_2 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', ',11', '12', '13', '14', '15', '16']
+    #     testdict_1 = {}
+    #
+    #     for fq_pairs in range(len(testlist_1)):
+    #         testdict_1[testlist_1[fq_pairs]] = testlist_2[fq_pairs]
+    #
+    #     methoddict_1 = build_dataset._pair_reference_fastq_files(testlist_1, testlist_2)
+    #
+    #     self.assertEqual(testdict_1, methoddict_1,
+    #                      "Dictionaries of equal size and containing equal long lists are not equals!")
+    #
+    # def test_create_synthesized_dataset_from_database(self):
+    #     background_bam = test_path + 'hg38/NA12878_mybam.sorted.bam'
+    #     background_fastq1 = test_path + 'mutacc_NA12878_mybam_R1.fastq.gz'
+    #     background_fastq2 = test_path + 'mutacc_NA12878_mybam_R2.fastq.gz'
+    #     caselist = build_dataset._extract_case_ids(test_path + 'mutacc_config_files/mutacc_config.yaml')
+    #     case_db_configfile = test_path + 'mutacc_config_files/mutacc_config.yaml'
+    #     synth_db_configfile = test_path + 'mutacc_config_files/mutacc_synthDB_config.yaml'
+    #     randomized_list = random.sample(caselist, int(len(caselist) * 0.67))
+    #     build_dataset._create_synthesized_dataset_from_database(background_bam, background_fastq1, background_fastq2,
+    #                                                             caselist,
+    #                                                             case_db_configfile, randomized_list,
+    #                                                             synth_db_configfile)
 
     def test_create_randomized_dataset(self):
         background_bam = test_path + 'hg38/NA12878_mybam.sorted.bam'
@@ -51,9 +51,11 @@ class MyTestCase(unittest.TestCase):
         background_fastq2 = test_path + 'mutacc_NA12878_mybam_R2.fastq.gz'
         case_db_configfile = test_path + 'mutacc_config_files/mutacc_config.yaml'
         synth_db_configfile = test_path + 'mutacc_config_files/mutacc_synthDB_config.yaml'
+        ref_fastq_1 = test_path + 'mutacc_NA12767_mybam_R1.fastq.gz'
+        ref_fastq_2 = test_path + 'mutacc_NA12767_mybam_R2.fastq.gz'
 
         build_dataset.create_randomized_dataset(case_db_configfile, synth_db_configfile, background_bam,
-                                                background_fastq1, background_fastq2)
+                                                background_fastq1, background_fastq2, ref_fastq_1, ref_fastq_2)
 
 
 if __name__ == '__main__':
